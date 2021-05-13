c09a98603581602c55e0539fae87fd721a2ab91f
Yeva <evasuslovaa@gmail.com>
2021-05-13 20:25:36 +0300

    n

--- src/MVC/details.view.ts
+++ src/MVC/details.view.ts
@@ -1,12 +1,19 @@
+import {filesCatalog} from "./model"
 export class DetailsView {
-    renderDetals(id,details) {
+    renderDetals(id,details:filesCatalog) {
         for(const [k,v] of Object.entries(details)) {
-            // for(const i in v) {
-            //     console.log(v[i].title)
-            // }
+            
             for(const i in v) {
-               console.log(id)
+               if(id === v[i].title) {
+                   const title = this.createElement("div")
+               }
             }
         }
+    }
+    createElement(tag:string, className?: string, content?: string): HTMLElement {
+        const element: HTMLElement = document.createElement(tag)
+        if(className) element.classList.add(className)
+        if(content) element.textContent = content
+        return element
     }
 }
